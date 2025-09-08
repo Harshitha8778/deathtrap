9c3277879badc01ef30e69efbb5bd2cd1fd5b91b
Harshitha Vellanki <harshithavellanki@gmail.com>
2025-09-08 20:42:32 +0530

    pizza

---
+++ Day3.1.py
@@ -0,0 +1,30 @@
+#Nested elif statements
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+bill = 0
+#size
+if size == "S":
+    bill = 15
+    if pepperoni == "Y":
+        bill+=2
+    if extra_cheese=="Y":
+            bill+=1
+elif size == "M":
+    bill = 20
+    if pepperoni == "Y":
+        bill+=3
+    if extra_cheese=="Y":
+            bill+=1
+elif size == "L":
+    bill = 25
+    if pepperoni == "Y":
+        bill+=3
+    if extra_cheese=="Y":
+            bill+=1
+final_bill="Your final bill is: $"+str(bill)+"."
+print(final_bill)
+#
+
+
