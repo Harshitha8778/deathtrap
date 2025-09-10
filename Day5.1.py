3b4eae1b56603f5bd3ae745ca5e0255c8df69440
Harshitha Vellanki <harshithavellanki@gmail.com>
2025-09-11 01:18:27 +0530

    fizzbuzzy

--- Day5.1.py
+++ Day5.1.py
@@ -1,12 +1,9 @@
 for num in range(1,101):
-    a = str(num)
-    if num % 3 == 0:
-        if a == str(num):
-            a = "Fizz"
-    if num % 5 == 0:
-        if a == str(num):
-            a = "Buzz"
     if num % 3 == 0 and num % 5 == 0:
-        if a == str(num):
-            a == "FizzBuzz"
-    print(a)
+         print("FizzBuzz")
+    elif num % 3 == 0:
+            print("Fizz")
+    elif num % 5 == 0:
+          print("Buzz")
+    else:
+          print(num)
